d60d364b8505d848d4b0ca67f6a364bd175d98bc
Ayo <Ayoroq@gmail.com>
2026-01-02 15:36:26 -0500

    added the conditional homepage based on user's login status

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,17 @@
 import App from '../App.jsx';
 import Home from '../pages/Home.jsx'
 import AuthHome from '../pages/AuthHome.jsx'
+import { useAuth } from '../context/AuthContext.jsx';
 
+function ConditionalHome() {
+    const { user } = useAuth();
+    
+    if (user) {
+        return <AuthHome />
+    } else {
+        return <Home />
+    }
+}
 
 const routes = [
     {
