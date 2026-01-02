725b2013cb930954b3baf46d964ae810d612916f
Ayo <Ayoroq@gmail.com>
2026-01-02 16:57:37 -0500

    Added basic navbar and some stylings to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-export default function Navbar(){
-    return(
-       <nav>
-        
-       </nav>
-    )
+import { useNavigate } from "react-router";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo} onClick={() => navigate('/')}>
+        <p>Uploader</p>
+      </div>
+      <div className={styles.navbarbuttons}>
+        <button className={`${styles.navbarbutton}`} onClick={() => navigate("/login")}>Login</button>
+        <button className={`${styles.navbarbutton}`} onClick={() => navigate("/signup")}>Signup</button>
+      </div>
+    </nav>
+  );
 }
