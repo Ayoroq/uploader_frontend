f046333bde2ff0b7e4c65d8ee2388fa3ddf6ca6b
Ayo <Ayoroq@gmail.com>
2026-01-04 23:58:05 -0500

    Added the button to upload files and hiding the input

--- src/pages/AuthHome.jsx
+++ src/pages/AuthHome.jsx
@@ -1,8 +1,15 @@
+import styles from './AuthHome.module.css'
+import {useRef} from 'react'
 export default function AuthHome() {
+    const fileInput = useRef(null)
     return (
-        <div>
-            <h1>Auth Home</h1>
-            <p>You are authenticated. You can access this page.</p>
-        </div>
+        <main className={styles.authhome}>
+            <section>
+                <button className={styles.upload} onClick={() => fileInput.current.click()}>Files Upload</button>
+                <input type="file" id='fileInput' name='fileInput' ref={fileInput} className={styles.fileInput} />
+            </section>
+            <section>
+            </section>
+        </main>
     )
 }   
