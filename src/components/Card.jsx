2da78771eea24be848bc8df3050bca5de439e35d
Ayo <Ayoroq@gmail.com>
2026-01-09 16:16:20 -0500

    Added more stylings to the mobile view

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,6 +5,7 @@ import {
 } from "../utils/utils.js";
 import folderIcon from "/assets/Folder.svg";
 import styles from "./Card.module.css";
+import moreIcon from "/assets/more.svg";
 
 export default function Card({
   fileFolder,
@@ -53,9 +54,12 @@ export default function Card({
           </p>
         </div>
       </div>
-      {fileFolder.type === "file" && (
-        <button onClick={handleDownload}>Download</button>
-      )}
+      <div className={styles.moreContainer }>
+        <button className={styles.more}>
+            {/* <img className={styles.moreIcon} src={moreIcon} alt="More" /> */}
+            <svg className={styles.moreIcon} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#1f1f1f"><path d="M207.86-432Q188-432 174-446.14t-14-34Q160-500 174.14-514t34-14Q228-528 242-513.86t14 34Q256-460 241.86-446t-34 14Zm272 0Q460-432 446-446.14t-14-34Q432-500 446.14-514t34-14Q500-528 514-513.86t14 34Q528-460 513.86-446t-34 14Zm272 0Q732-432 718-446.14t-14-34Q704-500 718.14-514t34-14Q772-528 786-513.86t14 34Q800-460 785.86-446t-34 14Z"/></svg>
+        </button>
+      </div>
     </div>
   );
 }
