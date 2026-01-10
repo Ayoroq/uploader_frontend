057ace33a988ace7a6dd3cf9d5b2d8811c47c1a8
Ayo <Ayoroq@gmail.com>
2026-01-09 23:47:19 -0500

    Added the lottie to show empty folders

--- src/components/MobileView.jsx
+++ src/components/MobileView.jsx
@@ -2,6 +2,7 @@ import styles from "./MobileView.module.css";
 import Card from "./Card.jsx";
 import filterIcon from "/assets/filter.svg";
 import backIcon from "/assets/back.svg";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 export default function MobileView({
   className,
   fileInput,
@@ -16,41 +17,56 @@ export default function MobileView({
 }) {
   return (
     <main className={`${styles.mobileView} ${className || ""}`}>
-        {folderPath && (<>
-            <div>
-                <button className={styles.backButton} onClick={() => navigate(-1)}>
-                    <img className={styles.backIcon} src={backIcon} alt="back" />
-                </button>
-            </div>
-            <div>
-                <p className={styles.folderName}>{folderPath.at(-1).name}</p>
-            </div>
-        </>)}
-        {
-            
-        }
-      {filesFolders.length > 0 && <nav className={styles.nav}>
-        <div>
-          <button className={styles.name}>Name</button>
-          <div className={styles.dropDown}>
-            <button className={styles.sort}>File Size</button>
-            <button className={styles.sort}>Date Modified</button>
-            <button className={styles.sort}>Date Created</button>
-            <hr />
-            <button className={styles.sort}>A - Z</button>
-            <button className={styles.sort}> Z - A</button>
+      {folderPath && (
+        <>
+          <div className={styles.backButtonContainer}>
+            <button className={styles.backButton} onClick={() => navigate(-1)}>
+              <img className={styles.backIcon} src={backIcon} alt="back" />
+            </button>
           </div>
-        </div>
-        <div>
-          <button className={styles.filter}>
-            <img className={styles.filterIcon} src={filterIcon} alt="filter" />
-          </button>
-          <div className={styles.dropDown}>
-            <button>Grid</button>
-            <button>Table</button>
+          <div>
+            <p className={styles.folderName}>{folderPath.at(-1).name}</p>
           </div>
+        </>
+      )}
+      {filesFolders.length > 0 ? (
+        <nav className={styles.nav}>
+          <div>
+            <button className={styles.name}>Name</button>
+            <div className={styles.dropDown}>
+              <button className={styles.sort}>File Size</button>
+              <button className={styles.sort}>Date Modified</button>
+              <button className={styles.sort}>Date Created</button>
+              <hr />
+              <button className={styles.sort}>A - Z</button>
+              <button className={styles.sort}> Z - A</button>
+            </div>
+          </div>
+          <div>
+            <button className={styles.filter}>
+              <img
+                className={styles.filterIcon}
+                src={filterIcon}
+                alt="filter"
+              />
+            </button>
+            <div className={styles.dropDown}>
+              <button>Grid</button>
+              <button>Table</button>
+            </div>
+          </div>
+        </nav>
+      ) : (
+        <div className={styles.mobileEmptyState}>
+          <p className={styles.emptyText}>The Folder is empty</p>
+          <DotLottieReact
+            src="/assets/empty.lottie"
+            autoplay={true}
+            className={styles.emptyLottie}
+          />
+          <p>Upload files or create folders to access them from any device</p>
         </div>
-      </nav>}
+      )}
       <div className={styles.cards}>
         {filesFolders.map((fileFolder, index) => {
           return (
