721a79adc21aace94813be947ed3ccb6c2b8db0c
Ayo <Ayoroq@gmail.com>
2026-01-16 12:09:12 -0500

    Added more styling to the desktop view

--- src/components/DesktopView.jsx
+++ src/components/DesktopView.jsx
@@ -74,151 +74,147 @@ export default function DesktopView({
           onChange={handleFileChange}
         />
       </section>
-      <section className={styles.rightSide}>
-        <div className={styles.miniNav}>
-          <div className={styles.folderPath}>
-            <div>
-              {!folderPath && <p className={styles.myFilesRoot}>My Files</p>}
-              {folderPath && (
-                <div className={styles.breadCrumbs}>
-                  <button
-                    className={styles.breadCrumbsButton}
-                    onClick={() => navigate("/")}
-                  >
-                    My Files
-                  </button>
-                  {folderPath.length > 0 && <span> {">"} </span>}
-                  {folderPath.map((item, index) => (
-                    <div key={item.id}>
-                      <button
-                        className={styles.breadCrumbsButton}
-                        onClick={() => handleFolderClick(item.id)}
-                      >
-                        {item.name}
-                      </button>
-                      {index < folderPath.length - 1 && <span> {">"} </span>}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
+      <section className={styles.miniNav}>
+        <div className={styles.folderPath}>
           <div>
-            <input
-              type="search"
-              className={styles.desktopSearch}
-              placeholder="Search your files"
-              value={searchTerm}
-              onChange={handleInputChange}
-              autoFocus
+            {!folderPath && <p className={styles.myFilesRoot}>My Files</p>}
+            {folderPath && (
+              <div className={styles.breadCrumbs}>
+                <button
+                  className={styles.breadCrumbsButton}
+                  onClick={() => navigate("/")}
+                >
+                  My Files
+                </button>
+                {folderPath.length > 0 && <span> {">"} </span>}
+                {folderPath.map((item, index) => (
+                  <div key={item.id}>
+                    <button
+                      className={styles.breadCrumbsButton}
+                      onClick={() => handleFolderClick(item.id)}
+                    >
+                      {item.name}
+                    </button>
+                    {index < folderPath.length - 1 && <span> {">"} </span>}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <input
+            type="search"
+            className={styles.desktopSearch}
+            placeholder="Search your files"
+            value={searchTerm}
+            onChange={handleInputChange}
+            autoFocus
+          />
+        </div>
+      </section>
+      <section className={styles.contentContainer}>
+        {loading ? (
+          <div className={styles.loading}>
+            <DotLottieReact
+              src="/assets/loading.lottie"
+              loop={true}
+              autoplay={true}
+              className={styles.loadingLottie}
             />
           </div>
-        </div>
-        <div className={styles.contentContainer}>
-          {loading ? (
-            <div className={styles.loading}>
-              <DotLottieReact
-                src="/assets/loading.lottie"
-                loop={true}
-                autoplay={true}
-                className={styles.loadingLottie}
-              />
-            </div>
-          ) : filesFolders.length > 0 ? (
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  <th onClick={() => handleSortChange("name")}>
-                    <div className={styles.th}>
-                      Name{" "}
-                      {sortBy === "name" &&
-                        (sortOrder === "asc" ? (
-                          <img
-                            className={styles.sortIcon}
-                            src={ascIcon}
-                            alt="asc"
-                          />
-                        ) : (
-                          <img
-                            src={descIcon}
-                            className={styles.sortIcon}
-                            alt="desc"
-                          />
-                        ))}
-                    </div>
-                  </th>
-                  <th onClick={() => handleSortChange("date")}>
-                    <div className={styles.th}>
-                      Modified{" "}
-                      {sortBy === "date" &&
-                        (sortOrder === "asc" ? (
-                          <img
-                            className={styles.sortIcon}
-                            src={ascIcon}
-                            alt="asc"
-                          />
-                        ) : (
-                          <img
-                            src={descIcon}
-                            className={styles.sortIcon}
-                            alt="desc"
-                          />
-                        ))}
-                    </div>
-                  </th>
-                  <th onClick={() => handleSortChange("size")}>
-                    <div className={styles.th}>
-                      File Size{" "}
-                      {sortBy === "size" &&
-                        (sortOrder === "asc" ? (
-                          <img
-                            className={styles.sortIcon}
-                            src={ascIcon}
-                            alt="asc"
-                          />
-                        ) : (
-                          <img
-                            src={descIcon}
-                            className={styles.sortIcon}
-                            alt="desc"
-                          />
-                        ))}
-                    </div>
-                  </th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {filesFolders.map((item) => (
-                  <FileTableRow
-                    key={item.id}
-                    item={item}
-                    onFolderClick={handleFolderClick}
-                    onFilePreview={previewFile}
-                    onDownload={handleDownload}
-                    onShare={handleShare}
-                    onRename={handleRename}
-                    onDelete={handleDelete}
-                    setItem={setItem}
-                    renameRef={renameRef}
-                  />
-                ))}
-              </tbody>
-            </table>
-          ) : (
-            <div className={styles.emptyState}>
-              <p className={styles.emptyText}>The Folder is empty</p>
-              <DotLottieReact
-                src="/assets/empty.lottie"
-                autoplay={true}
-                className={styles.emptyLottie}
-              />
-              <p>
-                Upload files or create folders to access them from any device
-              </p>
-            </div>
-          )}
-        </div>
+        ) : filesFolders.length > 0 ? (
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th onClick={() => handleSortChange("name")}>
+                  <div className={styles.th}>
+                    Name{" "}
+                    {sortBy === "name" &&
+                      (sortOrder === "asc" ? (
+                        <img
+                          className={styles.sortIcon}
+                          src={ascIcon}
+                          alt="asc"
+                        />
+                      ) : (
+                        <img
+                          src={descIcon}
+                          className={styles.sortIcon}
+                          alt="desc"
+                        />
+                      ))}
+                  </div>
+                </th>
+                <th onClick={() => handleSortChange("date")}>
+                  <div className={styles.th}>
+                    Modified{" "}
+                    {sortBy === "date" &&
+                      (sortOrder === "asc" ? (
+                        <img
+                          className={styles.sortIcon}
+                          src={ascIcon}
+                          alt="asc"
+                        />
+                      ) : (
+                        <img
+                          src={descIcon}
+                          className={styles.sortIcon}
+                          alt="desc"
+                        />
+                      ))}
+                  </div>
+                </th>
+                <th onClick={() => handleSortChange("size")}>
+                  <div className={styles.th}>
+                    File Size{" "}
+                    {sortBy === "size" &&
+                      (sortOrder === "asc" ? (
+                        <img
+                          className={styles.sortIcon}
+                          src={ascIcon}
+                          alt="asc"
+                        />
+                      ) : (
+                        <img
+                          src={descIcon}
+                          className={styles.sortIcon}
+                          alt="desc"
+                        />
+                      ))}
+                  </div>
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {filesFolders.map((item) => (
+                <FileTableRow
+                  key={item.id}
+                  item={item}
+                  onFolderClick={handleFolderClick}
+                  onFilePreview={previewFile}
+                  onDownload={handleDownload}
+                  onShare={handleShare}
+                  onRename={handleRename}
+                  onDelete={handleDelete}
+                  setItem={setItem}
+                  renameRef={renameRef}
+                />
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className={styles.emptyState}>
+            <p className={styles.emptyText}>The Folder is empty</p>
+            <DotLottieReact
+              src="/assets/empty.lottie"
+              autoplay={true}
+              className={styles.emptyLottie}
+            />
+            <p>Upload files or create folders to access them from any device</p>
+          </div>
+        )}
       </section>
       <RenameDialog
         ref={renameRef}
