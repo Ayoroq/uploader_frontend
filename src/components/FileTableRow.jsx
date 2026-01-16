e524be2c0209826304f9a3020730d9da947784e8
Ayo <Ayoroq@gmail.com>
2026-01-16 14:54:06 -0500

    Added the rename icon in the right place

--- src/components/FileTableRow.jsx
+++ src/components/FileTableRow.jsx
@@ -3,6 +3,7 @@ import folderIcon from '/assets/Folder.svg';
 import shareIcon from '/assets/share.svg';
 import downloadIcon from '/assets/download.svg';
 import deleteIcon from '/assets/delete.svg';
+import renameIcon from '/assets/rename.svg'
 import { getFileExtension, convertDate, formatFileSize } from '../utils/utils.js';
 import { useState, useRef, useEffect, useLayoutEffect } from 'react';
 
@@ -77,9 +78,7 @@ export default function FileTableRow({ item, onFolderClick, onFilePreview, onRen
                 </button>
               )}
               <button onClick={() => { setItem(item); setShowMenu(false);renameRef.current.showModal() }} className={styles.menuButton}>
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.menuIcon}>
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="currentColor"/>
-                </svg>
+                <img src={renameIcon} alt="Rename" className={styles.menuIcon} />
                 Rename
               </button>
               {item.type === "folder" ? null : (
