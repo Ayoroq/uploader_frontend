36435e910e0b6ab4ab2baea08a3e0836e9b313d6
Ayo <Ayoroq@gmail.com>
2026-01-16 11:09:45 -0500

    added more stylings to the table data

--- src/components/FileTableRow.jsx
+++ src/components/FileTableRow.jsx
@@ -45,33 +45,37 @@ export default function FileTableRow({ item, onFolderClick, onFilePreview, onRen
           `${item.type === "folder" ? styles.folder : styles.file} ${styles.rowItem}`
         }
       >
-        {item.type === "folder" ? (
-          <img src={folderIcon} alt="Folder" className={styles.icon}  />
-        ) : (
-          <img src={getFileExtension(item.name)} alt="File" className={styles.icon} />
-        )}
-        {item.name}
+        <div className={styles.nameCell}>
+          {item.type === "folder" ? (
+            <img src={folderIcon} alt="Folder" className={styles.icon}  />
+          ) : (
+            <img src={getFileExtension(item.name)} alt="File" className={styles.icon} />
+          )}
+          {item.name}
+        </div>
       </td>
-      <td>{convertDate(item.updatedAt)}</td>
+      <td><div className={styles.dateCell}>{convertDate(item.updatedAt)}</div></td>
       {item.type === "folder" ? (
-        <td>{item._count.files + item._count.subfolders}</td>
+        <td><div className={styles.sizeCell}>{item._count.files + item._count.subfolders}</div></td>
       ) : (
-        <td>{formatFileSize(item.size)}</td>
+        <td><div className={styles.sizeCell}>{formatFileSize(item.size)}</div></td>
       )}
       <td className={styles.moreCell}>
-        <button onClick={() => {setShowMenu(!showMenu)}} ref={moreButtonRef} className={styles.more}>
-          <svg className={styles.moreIcon} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#1f1f1f"><path d="M207.86-432Q188-432 174-446.14t-14-34Q160-500 174.14-514t34-14Q228-528 242-513.86t14 34Q256-460 241.86-446t-34 14Zm272 0Q460-432 446-446.14t-14-34Q432-500 446.14-514t34-14Q500-528 514-513.86t14 34Q528-460 513.86-446t-34 14Zm272 0Q732-432 718-446.14t-14-34Q704-500 718.14-514t34-14Q772-528 786-513.86t14 34Q800-460 785.86-446t-34 14Z"/></svg>
-        </button>
-        {showMenu && (
-          <div ref={menuRef} className={`${styles.moreMenu} ${position === 'top' ? styles.menuTop : styles.menuBottom}`}>
-            {item.type === "folder" ? null : (
-              <button onClick={() => { onShare(item.id); setShowMenu(false); }}>Share</button>
-            )}
-            <button onClick={() => { setItem(item); setShowMenu(false);renameRef.current.showModal() }}>Rename</button>
-            {item.type === "folder" ? null : <button onClick={() => { onDownload(item.id); setShowMenu(false); }}>Download</button>}
-            <button onClick={() => { onDelete(item); setShowMenu(false); }}>Delete</button>
-          </div>
-        )}
+        <div className={styles.moreCellContent}>
+          <button onClick={() => {setShowMenu(!showMenu)}} ref={moreButtonRef} className={styles.more}>
+            <svg className={styles.moreIcon} xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#1f1f1f"><path d="M207.86-432Q188-432 174-446.14t-14-34Q160-500 174.14-514t34-14Q228-528 242-513.86t14 34Q256-460 241.86-446t-34 14Zm272 0Q460-432 446-446.14t-14-34Q432-500 446.14-514t34-14Q500-528 514-513.86t14 34Q528-460 513.86-446t-34 14Zm272 0Q732-432 718-446.14t-14-34Q704-500 718.14-514t34-14Q772-528 786-513.86t14 34Q800-460 785.86-446t-34 14Z"/></svg>
+          </button>
+          {showMenu && (
+            <div ref={menuRef} className={`${styles.moreMenu} ${position === 'top' ? styles.menuTop : styles.menuBottom}`}>
+              {item.type === "folder" ? null : (
+                <button onClick={() => { onShare(item.id); setShowMenu(false); }}>Share</button>
+              )}
+              <button onClick={() => { setItem(item); setShowMenu(false);renameRef.current.showModal() }}>Rename</button>
+              {item.type === "folder" ? null : <button onClick={() => { onDownload(item.id); setShowMenu(false); }}>Download</button>}
+              <button onClick={() => { onDelete(item); setShowMenu(false); }}>Delete</button>
+            </div>
+          )}
+        </div>
       </td>
     </tr>
   );
