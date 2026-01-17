9e12482f5f6a28dd33d469ee9f74fa13c35bfa1e
Ayo <Ayoroq@gmail.com>
2026-01-17 02:20:13 -0500

    styling the new homepage and some of the navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,35 @@
-import { useNavigate } from 'react-router';
-import styles from './Home.module.css';
+import { useNavigate } from "react-router";
+import styles from "./Home.module.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Home() {
   const navigate = useNavigate();
 
   return (
     <div className={styles.home}>
-      
+      <section className={styles.hero}>
+        <DotLottieReact src="/assets/upload.lottie" autoplay />
+      </section>
+      <section className={styles.details}>
+        <p className={styles.appName}>Uploader</p>
+        <button className={styles.signInButton} onClick={() => navigate('/login')}>Sign In</button>
+      </section>
 
       <footer className={styles.footer}>
         <div className={styles.footerContent}>
           <div className={styles.footerLinks}>
-            <a href="#" className={styles.footerLink}>About</a>
-            <a href="#" className={styles.footerLink}>Privacy Policy</a>
-            <a href="#" className={styles.footerLink}>Terms of Service</a>
-            <a href="#" className={styles.footerLink}>Contact</a>
+            <a href="#" className={styles.footerLink}>
+              About
+            </a>
+            <a href="#" className={styles.footerLink}>
+              Privacy Policy
+            </a>
+            <a href="#" className={styles.footerLink}>
+              Terms of Service
+            </a>
+            <a href="#" className={styles.footerLink}>
+              Contact
+            </a>
           </div>
           <p className={styles.copyright}>
             © {new Date().getFullYear()} Uploader. Part of The Odin Project.
